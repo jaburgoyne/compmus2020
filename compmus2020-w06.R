6bff7d5eadddf49d091fcb9d447bf288b3b25335
jaburgoyne <j.a.burgoyne@uva.nl>
2020-02-05 00:40:13 +0100

    Create compmus2020-w06.R

---
+++ compmus2020-w06.R
@@ -0,0 +1,25 @@
+# Install/update tidyverse and spotifyr (one time)
+
+# Load libraries (every time)
+
+library(tidyverse)
+library(spotifyr)
+
+# Set Spotify access variables (every time)
+
+Sys.setenv(SPOTIFY_CLIENT_ID = 'YOUR_CLIENT_ID')
+Sys.setenv(SPOTIFY_CLIENT_SECRET = 'YOUR_CLIENT_SECRET')
+
+# Work with spotifyr. Note that playlists also require a username.
+
+juditha <-
+    get_track_audio_features(
+        c('2M5b9YLAgFroqWzeaZf86e', '3DBKc4ioGnMQLlbGQcFDIO'))
+alla <- get_album_tracks('7oI0E3DdTbD85rhMg19GSU')
+gilberto <- get_artist_audio_features('gilberto gil')
+disney <- get_playlist_audio_features('128899670', '5NtjgKz4doejP5HJtKXFcS')
+
+# Summarise key patterns
+
+gilberto %>% summarise(M = mean(danceability), SD = sd(danceability))
+disney %>% summarise(M = mean(danceability), SD = sd(danceability))
